4f59c5c03db22724372c7baee328de104dd351fc
Farid Ahmadian <ahmadian.farid.1988@gmail.com>
2022-03-17 14:14:57 +0100

    feat(core): Add the reporter

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import argparse
 from jinja2 import Environment, FileSystemLoader
 
 most_wanted = {"acl", "lifecycle_rule", "logging", "website", "policy"}
+suspects    = {}
+
 
 def investigator(file_name, dry_run):
   with open(file_name, 'r') as file:
@@ -15,10 +17,16 @@ def investigator(file_name, dry_run):
     for s3_bucket in s3_buckets:
       s3_bucket_name = next(iter( s3_bucket["aws_s3_bucket"]))
       for e in most_wanted.intersection(s3_bucket["aws_s3_bucket"][s3_bucket_name]):
-        print(f"Element `{e}` was found in `{s3_bucket_name}` bucket inside the `{file_name}`, Do something.")
+        suspects.setdefault(file_name, {})
+        suspects[file_name].setdefault(s3_bucket_name, [])
+        suspects[file_name][s3_bucket_name].append(e)
         logging.info(s3_bucket)
         if not dry_run:
             commentor(file_name, s3_bucket_name, e)
+
+def reporter():
+    print(f"It is going to change {len(suspects)} files, you can find the details here:")
+    print(suspects)
 
 def commentor(file_name, s3_bucket_name, wanted_element):
   logging.info(f"{file_name}, {s3_bucket_name}, {wanted_element}")
@@ -27,19 +35,21 @@ def commentor(file_name, s3_bucket_name, wanted_element):
     trim_blocks=True,
     lstrip_blocks=True)
   template = env.get_template(wanted_element + ".jinja2")
-  new_content = template.render(bucket_name=s3_bucket_name)
+  new_comment = template.render(bucket_name=s3_bucket_name)
   logging.info(new_content)
   with open(file_name, "a") as file_object:
-    file_object.write(new_content)
+    file_object.write(new_comment)
 
 def main(args):
   try:
-      for f in args.terraform_files:
-          logging.info(f"Investigating {f.name}")
-          investigator(f.name, args.dry_run)
+    for f in args.terraform_files:
+        logging.info(f"Investigating {f.name}")
+        investigator(f.name, args.dry_run)
+    print("finished")
+    reporter()
   except Exception as e:
-      logging.critical("Fatal error hapend: {}".format(e))
-      sys.exit(1)
+    logging.critical("Fatal error hapend: {}".format(e))
+    sys.exit(1)
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
